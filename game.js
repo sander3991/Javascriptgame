404ceeb50c98d44a274d313c22d085b0ae136ee9
henkertjan <henkjanmulder@live.nl>
2015-03-25 13:11:38 +0100

    Basis collisie detectie, hij is raak wanneer je een object recht raak schiet

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 $(function(){
-    var canvas = $("canvas")
+    var canvas = $("canvas");
     var Box = Backbone.Model.extend({
         defaults: {
             x: 0,
@@ -7,7 +7,8 @@ $(function(){
             w: 25,
             h: 25,
             color: "#FF9000",
-            linewidth: 3
+            linewidth: 3,
+            alive: true,
             // don't define a default id, that leads to strange behaviors
         }
     });
@@ -54,7 +55,7 @@ $(function(){
             var ctx = this.ctx;
             ctx.clearRect(0, 0, this.el.width, this.el.height);
             this.collection.each(function(model) {
-                if(!model.view) model.view = new BoxView({ctx: ctx, model: model});
+                if (!model.view) model.view = new BoxView({ ctx: ctx, model: model });
                 model.view.render();
             });
             this.shotCollection.each(function(model){
@@ -68,32 +69,38 @@ $(function(){
     var c = new BoxSet();
     c.add(player);
     c.add(new Box({x: 150, y: 150}));
-    c.add(new Box({x: 10, y: 10}));
-
+    c.add(new Box({ x: 10, y: 10 }));
 
     var counter = 0;
     /* Henk Jan */
-    $(document).keydown(function(e){
-        switch(e.which){
 
+    $(document).keydown(function (e) {
+        switch(e.which){
             case 37: //links
             case 65: //a
-                player.set({x: player.get("x") - 25});
+                player.set({x: player.get("x") - 10});
+                
                 break;
             case 38: //boven
             case 87: //u
-                player.set({y: player.get("y") - 25});
+                player.set({y: player.get("y") - 10});
                 break;
             case 39: //rechts
             case 68: //d
-                player.set({x: player.get("x") + 25});
+                player.set({ x: player.get("x") + 10 });
+                
                 break;
             case 40: //down
             case 83: //s
-                player.set({y: player.get("y") + 25});
+                player.set({y: player.get("y") + 10});
                 break;
         }
     });
+
+
+    function movePlayer(direction) {
+        player.set({ x: player.get("x") - 25 });
+    }
 
 
     /* Sander */
@@ -158,9 +165,32 @@ $(function(){
         var shot = new Shot({fromX: player.get("x") + 12.5, fromY: player.get("y") + 12.5, toX:e.offsetX, toY:e.offsetY});
         shootAudio.play();
         shots.add(shot);
+        // Check collisie 
+        checkCollision(shot, c);
+
         setTimeout(function(){
             shots.remove(shot);
         }, 100)
     });
+
+
+    function checkCollision(shot, c) {
+        //console.debug("piew", shot);
+        //console.debug("shot x", shot.get("toX"));
+        //console.debug("shot y", shot.get("toY"));
+
+        c.each(function (obj) {
+            for (x = 0; x < 25; x++) {
+                for (y = 0; y < 25; y++) {
+                    if ((obj.get("x") + x == shot.get("toX")) && (obj.get("y") + y == shot.get("toY"))) {
+                        console.debug("Raak");
+                    }   
+                }
+            }
+        });
+
+        
+    }
+    
     
 });
